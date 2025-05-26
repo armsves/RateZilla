cce3d8ec0977bcfb61939e71e2cae3c96dbf2054
Armando Medina <113925592+armsves@users.noreply.github.com>
2025-05-26 15:34:30 +0200

    deploying

--- packages/stellarContract/dist/index.js
+++ packages/stellarContract/dist/index.js
@@ -1,7 +1,7 @@
 import { Buffer } from "buffer";
 import { Client as ContractClient, Spec as ContractSpec, } from '@stellar/stellar-sdk/contract';
 export * from '@stellar/stellar-sdk';
-export * as contract from '@stellar/stellar-sdk/contract';
+export * as contract from '@stellar/stellar-sdk';
 export * as rpc from '@stellar/stellar-sdk';
 if (typeof window !== 'undefined') {
     //@ts-ignore Buffer exists
